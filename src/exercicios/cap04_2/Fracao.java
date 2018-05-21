a5a416c6ac9d9cf4fb9d7e131bc977fe90a123b1
marta <marta@marta-Latitude-E6400>
2018-05-21 16:17:55 -0300

    Cópia dos arquivos solução da Softblue. Exercício não resolvido.

--- src/exercicios/cap04_2/Fracao.java
+++ src/exercicios/cap04_2/Fracao.java
@@ -1,23 +1,54 @@
 package exercicios.cap04_2;
-
+/*
+ * Representa uma fra��o
+ */
 public class Fracao {
-	int numerador;
-	int denominador;
-	
-	Fracao fracao = new Fracao();
-	Fracao fracao2 = new Fracao();
 
-	void mult (int a, int b, int c, int d) {
-		fracao.numerador = a;
-		fracao.denominador = b;
-		fracao2.numerador = c;
-		fracao2.denominador = d;
+	//Numerador da fra��o
+	double numerador;
+	
+	//Denominador da fra��o
+	double denominador;
+	
+	//Calcula o valor do fra��o
+	double calcularValor() {
+		/*
+		 * Para calcular o valor, basta dividir o numerador da fra��o pelo denominador. Se o denominador 
+		 * for 0, o resultado � assumido como 0, j� que n�o � poss�vel fazer uma divis�o por 0.
+		 */
+		if(denominador == 0) {
+			return 0;
+		}
+		return numerador / denominador;
+	}
+	
+	/*
+	 * Multiplica a fra��o atual (do pr�prio objeto) por outra (passada como par�metro), dando uma terceira fra��o como resultado.
+	 */
+	Fracao multiplicar(Fracao f) {
+		//Cria um novo resultado
+		Fracao result = new Fracao();
 		
-		int mult_num = a * c;
-		int mult_den = b * d;
+		//Multiplica os numeradores e denominadores das fra��es
+		result.numerador = this.numerador * f.numerador;
+		result.denominador = this.denominador * f.denominador;
 		
-		System.out.println("O resultado da multiplicação " + a + "/" + b + "por" + c + "/" + d + "é:" + mult_num + "/" + mult_den);
-		
-		
+		//Retorna a nova fra��o
+		return result;
+	}
+	
+	/*
+	 * Define os valores do numerador e denominador da fra��o
+	 */
+	void definirValores(double numerador, double denominador) {
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
+	
+	/*
+	 * Retorna a representa��o da fra��o em forma de string
+	 */
+	String obterFracao() {
+		return numerador + "/" + denominador;
 	}
 }
