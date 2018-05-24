b8490653d0411606c49936401aef3f9c9f566d6d
marta <marta@marta-Latitude-E6400>
2018-05-24 12:08:38 -0300

    Exercício proposto 1, cap. 05.

---
+++ src/exercicio/proposto/cap05_1/Aplicacao.java
@@ -0,0 +1,45 @@
+/*
+ * Crie uma classe Lampada que possui um atributo ligada, o qual indica se a lâmpada está ligada 
+ou desligada. 
+Ao construir uma lâmpada, o estado dela (ligada ou desligada) deve ser fornecido. Para ligar e 
+desligar a lâmpada, os métodos ligar() e desligar() devem ser chamados, 
+respectivamente. Aliás, esta é a única forma de alterar o estado da lâmpada, já que o atributo 
+ligada não deve ser visível fora da classe. 
+A lâmpada também deve possuir um método imprimir(). Quando chamado, ele mostra as 
+mensagens “Lâmpada ligada” ou “Lâmpada desligada”, dependendo do estado atual. 
+Construa uma aplicação que cria uma lâmpada ligada, muda o estado dela e também imprime 
+o estado atual após cada chamada aos métodos ligar() e desligar(). 
+*/
+
+package exercicio.proposto.cap05_1;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Lampada l1 = new Lampada(false);
+		
+		Lampada l2 = new Lampada(true);
+		
+		Lampada l3 = new Lampada();   //Lampada que utiliza o construtor padrão, definido manualmente
+		
+		l3.ligar();
+		
+		l3.imprimir();		
+				
+		l1.imprimir();
+		
+		l2.imprimir();
+		
+		l2.desligar();
+		
+		l1.ligar();
+		
+		l1.imprimir();
+		
+		l2.imprimir();
+
+
+	}
+
+}
---
+++ src/exercicio/proposto/cap05_2/Aplicacao.java
@@ -0,0 +1,10 @@
+package exercicio.proposto.cap05_2;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
