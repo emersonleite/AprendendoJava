7581762a95b58c8fa258387c1a4220c446bc70a6
marta <marta@marta-Latitude-E6400>
2018-05-24 17:41:06 -0300

    Exercício 2 cap. 5.

--- src/exercicio/proposto/cap05_2/Aplicacao.java
+++ src/exercicio/proposto/cap05_2/Aplicacao.java
@@ -3,7 +3,20 @@ package exercicio.proposto.cap05_2;
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//int d = Data.constantes.FORMATO_12H;
+		
+		//System.out.println(d);
+		
+		Data d1 = new Data(01,01,13,12,00,01);
+		Data d3 = new Data(10,07,18);
+		
+		int d2 = d1.getAno();		
+		System.out.println(d2);
+		
+		int d4 = d3.getMes();
+		System.out.println(d4);
+		
 
 	}
 
